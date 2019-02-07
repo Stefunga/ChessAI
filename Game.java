509f97a61d07cbdd9e85721a119aa122e926640b
Stefunga <40221419+Stefunga@users.noreply.github.com>
2019-02-07 12:03:34 -0500

    Removed commented code

--- Game.java
+++ Game.java
@@ -50,7 +50,6 @@ public class Game {
 				else
 				{
 					System.out.printf("%nMove was made next player");
-				//	System.out.printf("%nThe board score:%s",check.getValue());
 					playGame(false,b);
 					return;
 				}	
@@ -62,35 +61,8 @@ public class Game {
 				MinMax a=new MinMax();
 				a.MiniMax(3,false, b,true,20);
 				a.getFinalBoard();
-				//a.getFinalBoard().printBoard();
-				//a.getFinalBoard().printBoard();
 				playGame(true,a.getFinalBoard());
 				return;
-				/**
-				System.out.printf("%nEnter piece initial location:ex 0 1 ");
-				int initLocX=in.nextInt();
-				int initLocY=in.nextInt();
-				if(b.getPiece(initLocX, initLocY)==null || b.getPiece(initLocX, initLocY).getColor()==true)
-				{
-					System.out.println("Invalid piece try again!");
-					playGame(false, b);
-				}
-				System.out.println("Enter piece destination:ex 0 1 ");
-				int destLocX=in.nextInt();
-				int destLocY=in.nextInt();
-				moveVerify v= new moveVerify(initLocX, initLocY, destLocX, destLocY, white, b);
-				Board check=v.changedBoard();
-				if(b.getPiece(initLocX, initLocY)!=null)
-				{
-					System.out.printf("%nMove was not made due to invalid input");
-					playGame(false,b);
-				}
-				else
-				{
-					System.out.printf("%nMove was made next player");
-					playGame(true,b);
-				}
-				*/
 			}
 		}
 	}
